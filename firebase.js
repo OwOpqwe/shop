e34c50af0b322a19f5cab18980dcd1dee98c4335
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-11 12:00:01 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+// FIREBASE CONFIG
+const firebaseConfig = {
+    apiKey: "AIzaSyDhzfNAuJWW09uiXm2sj3apnG48Y7MFLs4",
+    authDomain: "snack-store-ecc61.firebaseapp.com",
+    projectId: "snack-store-ecc61",
+    storageBucket: "snack-store-ecc61.firebasestorage.app",
+    messagingSenderId: "373410660807",
+    appId: "1:373410660807:web:9e16901f9602f05713d9e2",
+    measurementId: "G-P0EERKE117"
+};
+
+// INITIALIZE FIREBASE
+const app = initializeApp(firebaseConfig);
+
+// EXPORT AUTH + DATABASE
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
