1046284a3dcf8361e31fb39e12f7e42caa59572b
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-25 11:35:57 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-
-// FIREBASE CONFIG
 const firebaseConfig = {
-    apiKey: "AIzaSyDhzfNAuJWW09uiXm2sj3apnG48Y7MFLs4",
-    authDomain: "snack-store-ecc61.firebaseapp.com",
-    projectId: "snack-store-ecc61",
-    storageBucket: "snack-store-ecc61.firebasestorage.app",
-    messagingSenderId: "373410660807",
-    appId: "1:373410660807:web:9e16901f9602f05713d9e2",
-    measurementId: "G-P0EERKE117"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// INITIALIZE FIREBASE
 const app = initializeApp(firebaseConfig);
 
-// EXPORT AUTH + DATABASE
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
