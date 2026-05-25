76c4952823640b078dc4684dade00f7c4c5d5829
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-25 16:55:35 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,3 +19,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// 🔥 stabilizes login session
+setPersistence(auth, browserLocalPersistence).catch(console.error);
