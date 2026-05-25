ff36212add86590d3b15e09888770e1e61f83d7d
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-25 16:58:31 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,5 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// 🔥 stabilizes login session
 setPersistence(auth, browserLocalPersistence).catch(console.error);
