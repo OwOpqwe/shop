a38be98b8c368b5870ce6a328ce719db93d90722
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-25 16:30:37 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDhzfNAuJWW09uiXm2sj3apnG48Y7MFLs4",
+  authDomain: "snack-store-ecc61.firebaseapp.com",
+  projectId: "snack-store-ecc61",
+  storageBucket: "snack-store-ecc61.firebasestorage.app",
+  messagingSenderId: "373410660807",
+  appId: "1:373410660807:web:9e16901f9602f05713d9e2",
+  measurementId: "G-P0EERKE117"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export Auth + Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
