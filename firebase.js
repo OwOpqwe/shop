e7d204ba16c834b4299e43ee6b37343ecaf28b44
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-25 16:35:03 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDhzfNAuJWW09uiXm2sj3apnG48Y7MFLs4",
   authDomain: "snack-store-ecc61.firebaseapp.com",
   projectId: "snack-store-ecc61",
-  storageBucket: "snack-store-ecc61.appspot.com",
+  storageBucket: "snack-store-ecc61.firebasestorage.app",
   messagingSenderId: "373410660807",
   appId: "1:373410660807:web:9e16901f9602f05713d9e2",
   measurementId: "G-P0EERKE117"
