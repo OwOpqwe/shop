3eb008d59b28c1e3c4933f7cbd22a63d027cfdc1
s234js <148770100+OwOpqwe@users.noreply.github.com>
2026-05-26 16:40:37 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,45 +1,28 @@
-// =========================
-// FILE: firebase.js
-// =========================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
 import {
-    initializeApp,
-    getApps,
-    getApp
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-
-import {
-    getAuth,
-    setPersistence,
-    browserLocalPersistence
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 import {
     getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
 // FIREBASE CONFIG
 const firebaseConfig = {
     apiKey: "AIzaSyDhzfNAuJWW09uiXm2sj3apnG48Y7MFLs4",
     authDomain: "snack-store-ecc61.firebaseapp.com",
     projectId: "snack-store-ecc61",
-    storageBucket: "snack-store-ecc61.appspot.com",
+    storageBucket: "snack-store-ecc61.firebasestorage.app",
     messagingSenderId: "373410660807",
     appId: "1:373410660807:web:9e16901f9602f05713d9e2",
     measurementId: "G-P0EERKE117"
 };
 
-// PREVENT DUPLICATE APP
-const app =
-    getApps().length > 0
-        ? getApp()
-        : initializeApp(firebaseConfig);
+// INITIALIZE FIREBASE
+const app = initializeApp(firebaseConfig);
 
+// EXPORT AUTH + DATABASE
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 
-// KEEP LOGIN
-setPersistence(
-    auth,
-    browserLocalPersistence
-).catch(console.error);
+export const db = getFirestore(app);
